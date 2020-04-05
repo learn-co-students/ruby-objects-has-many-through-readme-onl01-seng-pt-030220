8665711b174f32709654a59f164efd0f4ed6cc3c
github username <github email address>
2020-04-05 05:41:53 -0500

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,16 @@
 class Meal
 
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    attr_accessor :waiter, :customer, :total, :tip
+
+    def initialize(waiter, customer, total, tip=0)
+        @waiter, @customer, @total, @tip = waiter, customer, total, tip
+        @@all << self
+    end
+
 end
